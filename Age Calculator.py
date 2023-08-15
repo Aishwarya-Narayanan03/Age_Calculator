7e7c324800035d40448f95187829654b2c0c315f
Aishwarya Narayanan <133395050+Aishwarya-Narayanan03@users.noreply.github.com>
2023-08-15 17:14:21 +0530

    Update Age Calculator.py

--- Age Calculator.py
+++ Age Calculator.py
@@ -9,20 +9,21 @@ def calculate_age():
         age = today.year - birthdate_date.year - ((today.month, today.day) < (birthdate_date.month, birthdate_date.day))
         label_result.config(text=f"You are {age} years old.")
     except ValueError:
-        label_result.config(text="Invalid date format. Please use YYYY-MM-DD.")
+        label_result.config(text=f"Hey we got it right! You are {age} years old.")
+
 
 # Create the main window
 root = tk.Tk()
 root.title("Age Calculator")
 
 # Create and place widgets
-label_instruction = tk.Label(root, text="Enter your birthdate (YYYY-MM-DD):")
+label_instruction = tk.Label(root, text="Type your DOB (YYYY-MM-DD):")
 label_instruction.pack(pady=10)
 
 entry_birthdate = tk.Entry(root)
 entry_birthdate.pack(pady=5)
 
-button_calculate = tk.Button(root, text="Calculate Age", command=calculate_age)
+button_calculate = tk.Button(root, text="What is my age", command=calculate_age)
 button_calculate.pack(pady=10)
 
 label_result = tk.Label(root, text="")
